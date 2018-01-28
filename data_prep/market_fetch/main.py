251e1027481c53859e0d58e4530edb55b8ae7033
Vincent Tan <vincnttan@gmail.com>
2018-01-28 20:54:42 +0000

    basic sort coin market cap by exchange

--- data_prep/market_fetch/main.py
+++ data_prep/market_fetch/main.py
@@ -3,6 +3,7 @@ import json
 import pprint
 import datetime
 import urllib.request
+import operator
 
 # location of existing list of coins
 coinlist_location = "https://raw.githubusercontent.com/vincnt/Lambo/master/coinlist.json"
@@ -12,6 +13,12 @@ coinlist_location = "https://raw.githubusercontent.com/vincnt/Lambo/master/coinl
 def read_current():
     with urllib.request.urlopen(coinlist_location) as url:
         data = json.loads(url.read().decode())
+        return data
+
+
+def read_local():
+    with open("/home/vincent/Projects/Crypto/Lambo/coinlist.json") as currentlist:
+        data = json.load(currentlist)
         return data
 
 
@@ -32,12 +39,45 @@ def cc_price(cc_symbol):
     pprint.pprint(data)
 
 
+def market_finder(coinlist):
+    kulist=[]
+    for x in coinlist:
+        if 'CC_markets' in coinlist[x]:
+            if coinlist[x]['CC_markets']['BTC']:
+                if 'Kucoin' in coinlist[x]['CC_markets']["BTC"]:
+                    kulist.append(coinlist[x]['CMC_ID'])
+    print(kulist)
+    print(len(kulist))
+    return kulist
+
+
+def cap_finder(coinlist):
+    caplist={}
+    response = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=0")
+    data = response.json()
+    for x in coinlist:
+        for y in data:
+            if y['id'] == x:
+                if y['market_cap_usd']:
+                    caplist[x] = float(y['market_cap_usd'])/1000000
+    return caplist
+
 
 def lambda_test(event,context):
 
     return event['test']
 
 
-coinlist = read_current()
+# Either read_local or read_current from github
+coinlist = read_local()
+
+# get latest price for coin from CC (10 sec accuracy)
 cc_symbol = coinlist["BTC"]["CC_symbol"]
 cc_price(cc_symbol)
+
+# get market cap of coins for an exchange (a few minutes accuracy)
+kulist = market_finder(coinlist)
+kulistd = cap_finder(kulist)
+sortedku = sorted(kulistd.items(), key=operator.itemgetter(1))
+for x in sortedku:
+    print(x)
